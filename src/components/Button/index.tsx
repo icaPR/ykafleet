679d8c2f8265da4ecef445e6ebe2ac8a671c8184
Icaro Ramos <polosnramos@hotmail.com>
2024-03-22 22:30:00 -0300

    fix: Fix properties in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ type Props = TouchableOpacityProps & {
 
 export function Button({ isLoading = false, title, ...rest }: Props) {
   return (
-    <Container activeOpacity={0.7} disabled={isLoading}>
+    <Container activeOpacity={0.7} disabled={isLoading} {...rest}>
       {isLoading ? <Loading /> : <Title>{title}</Title>}
     </Container>
   );
