07c0ceab8960b6d3215fc4ca457032634d809b47
Icaro Ramos <polosnramos@hotmail.com>
2024-03-23 16:55:05 -0300

    feat: Integrate react-native-safe-area in HomeHeader component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -5,17 +5,21 @@ import { useUser, useApp } from "@realm/react";
 import theme from "../../theme";
 
 import { Container, Greeting, Message, Name, Picture } from "./styles";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export function HomeHeader() {
   const user = useUser();
   const app = useApp();
+  const insets = useSafeAreaInsets();
+
+  const paddingTop = insets.top + 32;
 
   function handleLogout() {
     app.currentUser?.logOut();
   }
 
   return (
-    <Container>
+    <Container style={{ paddingTop }}>
       <Picture
         source={{ uri: user?.profile.pictureurl }}
         placeholder="L184i9kCbIof00ayjZay~qj[ayj@"
