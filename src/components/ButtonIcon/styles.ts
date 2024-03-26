dfdc6424fcc426f6d502dab994fad3707781da1e
Icaro Ramos <polosnramos@hotmail.com>
2024-03-25 21:45:57 -0300

    feat: Create ButtonIcon component

---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Container = styled.TouchableOpacity`
+  width: 56px;
+  height: 56px;
+  border-radius: 6px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+`;
