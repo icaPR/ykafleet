a25032a3101b5b91cdd549ac48a0e678dcf42410
Icaro Ramos <polosnramos@hotmail.com>
2024-03-25 22:04:32 -0300

    fix: Update typings of variables in schemas

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -13,8 +13,8 @@ export class Historic extends Realm.Object<Historic> {
   license_plate!: string;
   description!: string;
   status!: string;
-  created_at!: string;
-  updated_at!: string;
+  created_at!: Date;
+  updated_at!: Date;
   static generate({ user_id, license_plate, description }: GenerateProps) {
     return {
       _id: new Realm.BSON.UUID(),
