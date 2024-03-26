9e9ee2d069eed37b4878b77fadcb83a61ab4b6e4
Icaro Ramos <polosnramos@hotmail.com>
2024-03-25 21:56:35 -0300

    feat: Create handleRegister and fetchVehicleInUse functions

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,19 +2,58 @@ import { Container, Content } from "./styles";
 import { HomeHeader } from "../../components/HomeHeader";
 import { CarStatus } from "../../components/CarStatus";
 import { useNavigation } from "@react-navigation/native";
+import { useQuery, useRealm } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 
 export function Home() {
   const { navigate } = useNavigation();
+  const realm = useRealm();
+
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+
+  const historic = useQuery(Historic);
 
   function handleRegister() {
-    navigate("departure");
+    if (vehicleInUse?._id) {
+      return navigate("arrival", { id: vehicleInUse._id.toString() });
+    } else {
+      navigate("departure");
+    }
   }
+
+  function fetchVehicleInUse() {
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert(
+        "Veículo em uso",
+        "Não foi possível carregar veículo em uso.	"
+      );
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicleInUse();
+  }, []);
+
+  useEffect(() => {
+    realm.addListener("change", () => fetchVehicleInUse());
+
+    return () => realm.removeListener("change", () => fetchVehicleInUse);
+  }, []);
 
   return (
     <Container>
       <HomeHeader />
+
       <Content>
-        <CarStatus onPress={handleRegister} />
+        <CarStatus
+          onPress={handleRegister}
+          licensePlate={vehicleInUse?.license_plate}
+        />
       </Content>
     </Container>
   );
