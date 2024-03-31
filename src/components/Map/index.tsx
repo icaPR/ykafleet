7b39401ee00d67e1fdb890a207c86f88abb1b1f5
Icaro Ramos <polosnramos@hotmail.com>
2024-03-30 21:56:22 -0300

    feat: Create Map component

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,25 @@
+import MapView, {
+  LatLng,
+  MapViewProps,
+  PROVIDER_GOOGLE,
+} from "react-native-maps";
+
+type Props = MapViewProps & {
+  coordinates: LatLng[];
+};
+
+export function Map({ coordinates, ...rest }: Props) {
+  const lastCoordinate = coordinates[coordinates.length - 1];
+  return (
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={{ width: "100%", height: 200 }}
+      region={{
+        latitude: lastCoordinate.latitude,
+        longitude: lastCoordinate.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    />
+  );
+}
