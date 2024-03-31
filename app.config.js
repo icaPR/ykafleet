08908cf776eb8425b663f0b02f9e8f7005f092ac
Icaro Ramos <polosnramos@hotmail.com>
2024-03-30 21:47:43 -0300

    feat: Map Google Maps API key and update app.json to app.config.js file

---
+++ app.config.js
@@ -0,0 +1,51 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+module.exports = {
+  expo: {
+    name: "ykafleet",
+    slug: "ykafleet",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "cover",
+      backgroundColor: "#202024",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#202024",
+      },
+      package: "com.ykapr.ykafleet",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      "expo-font",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ],
+    ],
+  },
+};
--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -3,4 +3,5 @@ declare module "@env" {
   export const IOS_CLIENT_ID: string;
   export const WEB_CLIENT_ID: string;
   export const REALM_APP_ID: string;
+  export const GOOGLE_MAPS_API: string;
 }
